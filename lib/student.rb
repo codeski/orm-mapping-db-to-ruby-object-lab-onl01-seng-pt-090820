08b1fc45bc0e882ca9b5273d11657f7ecf4d6c61
Todd Stoneberg <todd.stoneberg@gmail.com>
2020-11-06 02:56:08 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,22 +1,38 @@
 class Student
   attr_accessor :id, :name, :grade
 
+  def self.create_table
+    sql = <<-SQL
+    CREATE TABLE IF NOT EXISTS students (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      grade TEXT
+    )
+    SQL
+
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql = "DROP TABLE IF EXISTS students"
+    DB[:conn].execute(sql)
+  end
+  
+  def save
+    sql = <<-SQL
+      INSERT INTO students (name, grade) 
+      VALUES (?, ?)
+    SQL
+
+    DB[:conn].execute(sql, self.name, self.grade)
+  end
+  
   def self.new_from_db(row)
     student = Student.new 
     student.id = row[0]
     student.name = row[1]
     student.grade = row[2]
     student
-  end
-
-  def self.all
-    sql = <<-SQL 
-    SELECT * FROM students
-    SQL
-    
-    DB[:conn].execute(sql).map do |row|
-       self.new_from_db(row)
-    end
   end
 
   def self.find_by_name(name)
@@ -47,6 +63,16 @@ class Student
     end
   end
   
+  def self.all
+    sql = <<-SQL 
+    SELECT * FROM students
+    SQL
+    
+    DB[:conn].execute(sql).map do |row|
+       self.new_from_db(row)
+    end
+  end
+
   def self.first_X_students_in_grade_10(x)
     sql = <<-SQL
     SELECT * FROM students WHERE grade = 10 LIMIT ?
@@ -76,30 +102,5 @@ class Student
       self.new_from_db(row)
     end
   end
-  
-  def save
-    sql = <<-SQL
-      INSERT INTO students (name, grade) 
-      VALUES (?, ?)
-    SQL
 
-    DB[:conn].execute(sql, self.name, self.grade)
-  end
-  
-  def self.create_table
-    sql = <<-SQL
-    CREATE TABLE IF NOT EXISTS students (
-      id INTEGER PRIMARY KEY,
-      name TEXT,
-      grade TEXT
-    )
-    SQL
-
-    DB[:conn].execute(sql)
-  end
-
-  def self.drop_table
-    sql = "DROP TABLE IF EXISTS students"
-    DB[:conn].execute(sql)
-  end
 end
